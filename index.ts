1ed1ca4b6bee2028d34efa6e32b135703e9a1682
JobPC <nikita.deki4@gmail.com>
2024-05-23 17:35:22 +0300

    third commit

--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
-const express = require("express");
+import express from "express";
 
 const app = express();
 
 const port = 3009;
 
 app.get("/", (req, res) => {
-  res.send("hello worlD!");
+  res.send("hello worl1445555544!");
 });
 
 app.listen(port, () => {
