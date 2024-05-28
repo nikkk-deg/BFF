503637d17043f5ce2cc14f2f1f0abf74c7aab85f
JobPC <nikita.deki4@gmail.com>
2024-05-28 10:55:10 +0300

    deploy

--- dist/2.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const http = require('http');
-const server = http.createServer().listen(3000);
-server.on('request', (req, res) => {
-    if (req.url === '/') {
-        return res.end('server is running');
-    }
-});
